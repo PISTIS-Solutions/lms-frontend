52469db2903d528960e0b59f5ba04e9c9fabdfcf
Israel-sanmi <adetomokunisrael2001@gmail.com>
2024-02-16 04:18:29 +0100

    mobile, tab, pc responsiveness

--- components/side-comp/testimony-card.tsx
+++ components/side-comp/testimony-card.tsx
@@ -12,15 +12,15 @@ interface Componentprops {
 
 const TestimonialCard = ({ avatar, name, quote }: Componentprops) => {
   return (
-    <main className="md:w-[409px] w-[345px] h-[276px] shadow-md relative rounded-[16px] p-4 bg-white">
+    <main className="md:w-[409px] w-[345px] md:h-[276px] shadow-md relative rounded-[16px] p-4 bg-white">
       <Image
         src={avatar}
         alt="avatar"
         priority
-        className="md:w-[108px] w-[100px] h-[100px] md:h-[108px] absolute right-5 -top-10"
+        className="md:w-[108px] w-[80px] h-[80px] md:h-[108px] absolute right-5 -top-5"
       />
       <div>
-        <h3 className="text-xl font-semibold">{name}</h3>
+        <h3 className="md:text-2xl sm:text-lg text-sm font-semibold">{name}</h3>
         <div className="flex gap-x-1 text-main">
           <Star />
           <Star />
@@ -30,7 +30,7 @@ const TestimonialCard = ({ avatar, name, quote }: Componentprops) => {
         </div>
       </div>
       <div>
-        <p className="md:text-lg text-base mt-10 text-[#3E3E3E]">“{quote}”</p>
+        <p className="md:text-xl text-xs sm:text-base mt-10 text-[#3E3E3E]">“{quote}”</p>
       </div>
     </main>
   );
